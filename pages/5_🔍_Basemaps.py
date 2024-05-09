9836ff517512a6d516e9103ccfa0e1e0fd2d4ee8
chaitanyaj14 <chaitanyaj14@gmail.com>
2024-05-09 12:47:27 +0530

    updated links

--- pages/5_🔍_Basemaps.py
+++ pages/5_🔍_Basemaps.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 markdown = """
 A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+<https://github.com/chaitanyaj14/map-template>
 """
 
 st.sidebar.title("About")
